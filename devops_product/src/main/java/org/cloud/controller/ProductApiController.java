c6037805ff4b9eceeed7bb9ddc64a5d80f6742d1
user <user@DESKTOP-RMCGQTK.mshome.net>
2026-04-09 09:56:40 +0900

    devops1.1

---
+++ devops_product/src/main/java/org/cloud/controller/ProductApiController.java
@@ -0,0 +1,53 @@
+package org.cloud.controller;
+
+import java.util.List;
+
+import org.cloud.dto.ProductDTO;
+import org.cloud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/product")
+public class ProductApiController {
+
+	@Autowired	
+	private ProductService productService;
+	
+	@GetMapping("/list")
+	public List<ProductDTO> openProductList() throws Exception {
+		return productService.productList();
+	}
+	
+	@PostMapping("/insert")
+	public String insertProduct(@RequestBody ProductDTO product) throws Exception {
+		productService.insertProduct(product);
+		return "insert success";
+	}
+	
+	@GetMapping("/detail/{num}")
+	public ProductDTO openProductDetail(@PathVariable("num") int num) throws Exception {
+		return productService.productDetail(num);
+	}
+	
+	@PutMapping("/update")
+	public String updateProduct(@RequestBody ProductDTO product) throws Exception {
+		productService.updateProduct(product);
+		return "update success";
+	}
+	
+	@DeleteMapping("/delete/{num}")
+	public String deleteProduct(@PathVariable("num") int num) throws Exception {
+		productService.deleteProduct(num);
+		return "delete success";
+	}
+}
