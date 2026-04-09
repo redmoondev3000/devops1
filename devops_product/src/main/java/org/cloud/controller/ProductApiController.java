6877cb8cd58768c01d750f1d486a1fec471cdc2d
redmoondev3000 <redmoon.dev3000@gmail.com>
2026-04-09 11:48:25 +0900

    v1

--- devops_product/src/main/java/org/cloud/controller/ProductApiController.java
+++ devops_product/src/main/java/org/cloud/controller/ProductApiController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-//github test pull
+//github test pull 
 @CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/product")
